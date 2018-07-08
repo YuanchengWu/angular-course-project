479ac6ee3e37ccd92f68e6f49aa8e05f594ff25b
Yuancheng Wu <johnwu0202@gmail.com>
2018-07-08 14:49:31 -0700

    add missing files

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://images.unsplash.com/photo-1512058454905-6b841e7ad132?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d1859a8354ebc4050a3c290fc57fa66f&auto=format&fit=crop&w=1586&q=80')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
