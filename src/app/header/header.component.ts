025c481c60a85cfca27c102eb752bce573691a85
Yuancheng Wu <johnwu0202@gmail.com>
2018-07-08 16:13:47 -0700

    implement custom event emitter so recipes show only when recipes tab clicked on navbar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() activeTab: EventEmitter<string> = new EventEmitter();
+  onRecipes() {
+    this.activeTab.emit('recipes');
+  }
 
+  onShoppingList() {
+    this.activeTab.emit('shopping list');
+  }
 }
