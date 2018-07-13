43b125e39a26dbe0c78325aa1e358f3cf05ca93c
Yuancheng Wu <johnwu0202@gmail.com>
2018-07-12 21:04:08 -0700

    add recipe detail only when recipe is clicked

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,12 +11,20 @@ export class RecipeListComponent implements OnInit {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
+      'https://images.unsplash.com/photo-1512058454905-6b841e7ad132?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d1859a8354ebc4050a3c290fc57fa66f&auto=format&fit=crop&w=1586&q=80'),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
       'https://images.unsplash.com/photo-1512058454905-6b841e7ad132?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d1859a8354ebc4050a3c290fc57fa66f&auto=format&fit=crop&w=1586&q=80')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
